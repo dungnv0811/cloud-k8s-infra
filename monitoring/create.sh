bf09b168fbb11c92f3b6f7541075dace68b2381c
dungnv0811 <dungnv.itdn@gmail.com>
2022-10-02 20:11:48 +0700

    update slack notification

--- monitoring/create.sh
+++ monitoring/create.sh
@@ -6,3 +6,4 @@ kubectl apply -f monitoring/node-exporter
 kubectl apply -f monitoring/alertmanager
 
 https://grafana.com/blog/2020/02/25/step-by-step-guide-to-setting-up-prometheus-alertmanager-with-slack-pagerduty-and-gmail/
+https://api.slack.com/apps/A044BP8SH39/incoming-webhooks?success=1
