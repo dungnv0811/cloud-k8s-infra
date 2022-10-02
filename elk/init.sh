b8046ed54376d16fe80cda92de339c9df60e8ec0
dungnv0811 <dungnv.itdn@gmail.com>
2022-10-02 11:12:59 +0700

    update elk and slack notification

--- elk/init.sh
+++ elk/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # index= logstash-*
-# discover -> add fields
+# discover -> add fields(loglevel) -> loglevel.keywords exists and loglevel is one of ...
+# visualize -> add vertical bar -> choose discover -> Split Series -> Terms -> loglevel
 https://coralogix.com/blog/kubernetes-logging-with-elasticsearch-fluentd-and-kibana/
---
+++ monitoring/alertmanager/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+https://grafana.com/blog/2020/02/25/step-by-step-guide-to-setting-up-prometheus-alertmanager-with-slack-pagerduty-and-gmail/
